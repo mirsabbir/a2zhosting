3d3007031e722bdb24e022138fbd0fe658b46008
mithun <mithunbaul2@gmail.com>
2018-09-28 21:41:22 +0600

    kuch bhi

--- database/migrations/2018_09_28_012957_add_published_column_reply.php
+++ database/migrations/2018_09_28_012957_add_published_column_reply.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPublishedColumn extends Migration
+class AddPublishedColumnReply extends Migration
 {
     /**
      * Run the migrations.
